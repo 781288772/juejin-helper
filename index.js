62dbf2a060e69cf48fe2b27887f843df93ba7064
781288772 <781288772@users.noreply.github.com>
2022-12-22 08:47:30 +0000

    Deploying to main from @ 781288772/juejin-helper@fc6f90c88e709499ee3d43b6966e84a18c09f918 🚀

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const { signIn } = require('./sign')
 const { growth } = require('./growth')
-const { closeBrowser } = require("./puppeteer/index"); 
-    (async () => {
+const { closeBrowser } = require("./puppeteer/index")
+    ; (async () => {
         await signIn()
         if (!process.env.NOT_GROWTH) {
             await growth()
